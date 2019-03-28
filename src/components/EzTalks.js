7c156b6cf2e2823ed2c8565078c3ed4c19e355c5
maykon <maykonmichelpalma@gmail.com>
2019-03-28 09:50:15 -0300

    Added alert to see how many times the component is re-render

--- src/components/EzTalks.js
+++ src/components/EzTalks.js
@@ -3,9 +3,12 @@ import { Image, View, Text } from 'react-native';
 
 import logo from '../../assets/images/logo.jpg';
 
-export default ({ title }) => (
-  <View style={{ borderColor: 'red', borderWidth: 5 }}>
-    <Image source={logo} />
-    <Text>{title}</Text>
-  </View>
-);
+export default ({ title }) => {
+  alert('Renderizando o componente EzTalks');
+  return (
+    <View style={{ borderColor: 'red', borderWidth: 5 }}>
+      <Image source={logo} />
+      <Text>{title}</Text>
+    </View>
+  );
+};
