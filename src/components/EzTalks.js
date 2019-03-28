09c106ea14c240cc5d0354b8609c02ded872d62a
maykon <maykonmichelpalma@gmail.com>
2019-03-28 09:37:14 -0300

    Removed state Downloaded logo

--- src/components/EzTalks.js
+++ src/components/EzTalks.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import { Image, View, Text } from 'react-native';
 
-export default class EzTalks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      logo: {
-        uri: 'https://yt3.ggpht.com/a-/AAuE7mAPlMUUv9nG7b0yk-3-' +
-          '8HIFlAPELHSYjxhdIQ=s288-mo-c-c0xffffffff-rj-k-no',
-        height: 288,
-        width: 288
-      }
-    };
-  }
+import logo from '../../assets/images/logo.jpg';
 
+export default class EzTalks extends React.Component {
   render() {
     const { title } = this.props;
-    const { logo } = this.state;
 
     return (
       <View style={{ borderColor: 'red', borderWidth: 5 }}>
