778efbbe665c24a0ad980c3a109cfe117e781374
maykon <maykonmichelpalma@gmail.com>
2019-03-28 09:45:11 -0300

    Added Strong component

---
+++ src/components/Strong.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default ({ children }) =>
+  <Text style={{
+    fontWeight: 'bold',
+    color: 'blue'
+  }}>
+    {children}
+  </Text>
