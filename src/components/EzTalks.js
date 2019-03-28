45ee9c3bb39761fe11fa6ceb96cd40257034097d
maykon <maykonmichelpalma@gmail.com>
2019-03-28 09:55:45 -0300

    Updated to pure component

--- src/components/EzTalks.js
+++ src/components/EzTalks.js
@@ -3,12 +3,15 @@ import { Image, View, Text } from 'react-native';
 
 import logo from '../../assets/images/logo.jpg';
 
-export default ({ title }) => {
-  alert('Renderizando o componente EzTalks');
-  return (
-    <View style={{ borderColor: 'red', borderWidth: 5 }}>
-      <Image source={logo} />
-      <Text>{title}</Text>
-    </View>
-  );
+export default class EzTalks extends React.PureComponent {
+  render() {
+    alert('Renderizando o componente EzTalks');
+    const { title } = this.props;
+    return (
+      <View style={{ borderColor: 'red', borderWidth: 5 }}>
+        <Image source={logo} />
+        <Text>{title}</Text>
+      </View>
+    );
+  }
 };
